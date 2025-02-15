087995f11313b76e40180662665a4ee6f2ca542f
Eben <kwabenat679@gmail.com>
2025-02-15 22:31:50 +0000

    added funciton for random colors

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ body.style.backgroundColor = "red"
 function setColor(name) {
     body.style.backgroundColor = name;
 }
+
+function randomColor() {
+    const red = Math.round (Math.random() * 255)
+    const green = Math.round (Math.random() * 255)
+    const blue = Math.round (Math.random() * 255)    
+}
